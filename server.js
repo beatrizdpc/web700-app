0f288ab30047b586c8c098999a16f476f2750df9
beatrizdpc <beatrizpaula01@hotmail.com>
2025-03-10 19:10:35 -0400

    First commit - assigment 4

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Beatriz Alves Student ID: 112419247 Date: 2024/02/18
+*
+********************************************************************************/ 
 const express = require("express");
 const path = require("path");
 const collegeData = require("./modules/collegeData.js");
